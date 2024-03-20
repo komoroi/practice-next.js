e7b305250f7cfc3bb1d15ebd0748e8dc0465dda1
komoroi <koheim2511@gmail.com>
2024-03-21 08:57:37 +0900

    2.1まで

--- src/app/_components/Nav/index.tsx
+++ src/app/_components/Nav/index.tsx
@@ -1,17 +1,48 @@
+// import Link from "next/link";
+// import styles from "./style.module.css";
+
+// export function Nav() {
+//     return (
+//         <nav className={styles.nav} >
+//             <ul>
+//                 <li>
+//                     <Link href="/">トップ</Link>
+//                 </li>
+//                 <li>
+//                     <Link href="/categories">カテゴリー一覧</Link>
+//                 </li>
+//             </ul>
+//         </nav>
+//     );
+// }
+
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./style.module.css";
+import { IconButton } from "./IconButton ";
+
+function getAriaCurrent(flag: boolean){
+    return flag ? {"aria-current": "page" as const} : undefined;
+}
 
 export function Nav() {
+    const pathName = usePathname(); 
+    const openModal = () => {
+        //モーダル開閉処理
+    }
     return (
-        <nav >
+        <nav className={styles.nav} >
             <ul>
                 <li>
-                    <Link href="/">トップ</Link>
+                    <Link href="/" {...getAriaCurrent(pathName === "/")}>トップ</Link>
                 </li>
                 <li>
-                    <Link href="/categories">カテゴリー一覧</Link>
+                    <Link href="/categories"
+                    {...getAriaCurrent(pathName.startsWith("/categories"))}>カテゴリー一覧</Link>
                 </li>
             </ul>
+            <IconButton onClick={openModal}>写真を投稿する</IconButton>
         </nav>
     );
 }
