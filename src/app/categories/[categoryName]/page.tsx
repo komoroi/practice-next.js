0516501ad241ea98d903ba80748f2b765fa41c5b
komoroi <koheim2511@gmail.com>
2024-03-20 23:28:31 +0900

    1章終わり

--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,16 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 type Props = {
-    params: { categoryName: string};
-    searchParams: { [key:string]: string | string[] | undefined };
+    params: { categoryName: string };
+    searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export default function Page({ params, searchParams }: Props) {
-    const page = typeof searchParams.page === "string" ? searchParams.page : "1"
-;
-    return(
+    const page = typeof searchParams.page === "string" ? searchParams.page : "1";
+
+    const router = useRouter();
+
+    return (
         <div>
             <h1>カテゴリー一覧</h1>
             <h2>カテゴリー「{params.categoryName}」</h2>
             <p>ページ番号：「{page}」</p>
+            <button
+                onClick={() => {
+                    router.push("/categories");
+                }}
+            >
+                カテゴリー一覧へ戻る
+            </button>
         </div>
     );
 }
